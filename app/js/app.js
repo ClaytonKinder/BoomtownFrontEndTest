9bbf798fdaf3e88c8b3b20defa8cf983d68c08d3
Clayton Kinder <ClaytonAlanKinder@gmail.com>
2015-09-09 20:00:34 -0400

    On my way.

---
+++ app/js/app.js
@@ -0,0 +1,5 @@
+console.log('Yo!');
+
+$('.uk-icon-bars').on('click', function(){
+  alert('You clicked me!');
+})
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,22 @@
 var gulp = require('gulp');
+var minifyCSS = require('gulp-minify-css');
+var less = require('gulp-less');
+var watch = require('gulp-watch');
+var prefix = require('gulp-autoprefixer');
+var plumber = require('gulp-plumber');
+var path = require('path');
 var browserSync = require('browser-sync');
+var livereload = require('gulp-livereload');
 var reload = browserSync.reload;
+
+gulp.task('less', function() {
+  return gulp.src('./app/styles/less/styles.less')  // only compile the entry file
+    .pipe(less())
+    .pipe(prefix("last 8 version", "> 1%", "ie 8", "ie 7"), {cascade:true})
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('./app/styles/css'))
+    .pipe(livereload());
+});
 
 // watch files for changes and reload
 gulp.task('serve', function() {
@@ -14,4 +30,9 @@ gulp.task('serve', function() {
   gulp.watch(['*.html', 'styles/**/*.less', 'styles/**/*.css', 'scripts/**/*.js'], {cwd: 'app'}, reload);
 });
 
-gulp.task('default', ['serve']);
+gulp.task('watchLess', function() {
+  // Watch all the .less files, then run the less task
+    gulp.watch('./**/*.less', ['less']);
+});
+
+gulp.task('default', ['serve', 'watchLess']);
