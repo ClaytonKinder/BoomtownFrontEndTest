3cf277760165a921bc156e79679286b035cdce36
Clayton Kinder <ClaytonAlanKinder@gmail.com>
2015-09-10 20:25:37 -0400

    Working on side navbar.

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,39 @@
-console.log('Yo!');
+// Loads all html files into index.html
+$(function(){
+  $("#navbarInclude").load("../views/navbar.html");
+  $("#sideNavbarInclude").load("../views/side-navbar.html");
+  $("#headerInclude").load("../views/header.html");
+  $("#mainUsersInclude").load("../views/main-users.html");
+  $("#mainWeatherInclude").load("../views/main-weather.html");
+  $("#mainChatInclude").load("../views/main-chat.html");
+  $("#mainUsageInclude").load("../views/main-usage.html");
+  $("#asideActivitiesInclude").load("../views/aside-activities.html");
+  $("#asideSettingsInclude").load("../views/aside-settings.html");
+  $("#asideTasksInclude").load("../views/aside-tasks.html");
+});
 
-$('#mobileMenuToggle').on('click', function(){
+$(window).load(function () {
+  // Controls the initial color of the custom checkboxes in the first section of the
+  // sidebar settings
+  $('.slideCheckbox').each(function(){
+    if ($(this)[0].checked === true) {
+      $(this).closest('.slideCheckboxBlock').css('background-color', '#679900');
+    } else {
+      $(this).closest('.slideCheckboxBlock').css('background-color', '#CCCCCC');
+    }
+  });
+});
+
+// Toggles the side-menu
+$('body').on('click', '#sideMenuToggle', function(){
   alert('You clicked me!');
+})
+
+// Controls the custom checkboxes in the first section of the sidebar settings
+$('body').on('change', '.slideCheckbox', function(){
+  if ($(this)[0].checked === true) {
+    $(this).closest('.slideCheckboxBlock').css('background-color', '#679900');
+  } else {
+    $(this).closest('.slideCheckboxBlock').css('background-color', '#CCCCCC');
+  }
 })
