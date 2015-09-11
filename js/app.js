bd147134741dd4a9bb86d40910a96912ec3534d9
Clayton Kinder <ClaytonAlanKinder@gmail.com>
2015-09-10 21:41:52 -0400

    Test

--- js/app.js
+++ js/app.js
@@ -1,18 +1,20 @@
 // Loads all html files into index.html
-$(function(){
-  $("#navbarInclude").load("../views/navbar.html");
-  $("#sideNavbarInclude").load("../views/side-navbar.html");
-  $("#headerInclude").load("../views/header.html");
-  $("#mainUsersInclude").load("../views/main-users.html");
-  $("#mainWeatherInclude").load("../views/main-weather.html");
-  $("#mainChatInclude").load("../views/main-chat.html");
-  $("#mainUsageInclude").load("../views/main-usage.html");
-  $("#asideActivitiesInclude").load("../views/aside-activities.html");
-  $("#asideSettingsInclude").load("../views/aside-settings.html");
-  $("#asideTasksInclude").load("../views/aside-tasks.html");
-});
 
 $(window).load(function () {
+
+  $(function(){
+    $("#navbarInclude").load("../views/navbar.html");
+    $("#sideNavbarInclude").load("../views/side-navbar.html");
+    $("#headerInclude").load("../views/header.html");
+    $("#mainUsersInclude").load("../views/main-users.html");
+    $("#mainWeatherInclude").load("../views/main-weather.html");
+    $("#mainChatInclude").load("../views/main-chat.html");
+    $("#mainUsageInclude").load("../views/main-usage.html");
+    $("#asideActivitiesInclude").load("../views/aside-activities.html");
+    $("#asideSettingsInclude").load("../views/aside-settings.html");
+    $("#asideTasksInclude").load("../views/aside-tasks.html");
+  });
+
   // Controls the initial color of the custom checkboxes in the first section of the
   // sidebar settings
   $('.slideCheckbox').each(function(){
