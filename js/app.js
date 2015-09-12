8dd086983cffa107239951c854e74165265564e7
Clayton Kinder <ClaytonAlanKinder@gmail.com>
2015-09-12 19:16:29 -0400

    Finished with first draft of responsiveness.

--- js/app.js
+++ js/app.js
@@ -1,32 +1,61 @@
-// Loads all html files into index.html
 
-$(window).load(function () {
-
-  // $(function(){
-  //   $("#navbarInclude").load("./views/navbar.html");
-  //   $("#sideNavbarInclude").load("./views/side-navbar.html");
-  //   $("#headerInclude").load("./views/header.html");
-  //   $("#mainUsersInclude").load("./views/main-users.html");
-  //   $("#mainWeatherInclude").load("./views/main-weather.html");
-  //   $("#mainChatInclude").load("./views/main-chat.html");
-  //   $("#mainUsageInclude").load("./views/main-usage.html");
-  //   $("#asideActivitiesInclude").load("./views/aside-activities.html");
-  //   $("#asideSettingsInclude").load("./views/aside-settings.html");
-  //   $("#asideTasksInclude").load("./views/aside-tasks.html");
-  // });
-});
 
 // Toggles the side-menu
 $('body').on('click', '#sideMenuToggle', function(){
   $('.sideNavbarBlock').toggleClass('sideNavbarOpen');
   $('.sideNavbarMask').toggleClass('sideNavbarOpen');
+  $('aside').toggleClass('sideNavbarOpen');
   $('.contentWrapper').toggleClass('sideNavbarOpen');
 })
 
+// Clicking the opaque area while side-menu is open closes the side-menu
 $('body').on('click', '.sideNavbarMask', function(){
   $('.sideNavbarBlock').removeClass('sideNavbarOpen');
   $('.sideNavbarMask').removeClass('sideNavbarOpen');
+  $('aside').removeClass('sideNavbarOpen');
   $('.contentWrapper').removeClass('sideNavbarOpen');
+})
+
+var asideMenuToggle = function(asideToggle, asideSection) {
+    var at = asideToggle;
+    var atSibs = asideToggle.siblings();
+    var aside = $('aside');
+
+    if (asideSection === 'activities') {
+      var as = $('section.activities');
+      var asSibs = as.siblings();
+    } else if (asideSection === 'tasks') {
+      var as = $('section.tasks');
+      var asSibs = as.siblings();
+    } else {
+      var as = $('section.settings');
+      var asSibs = as.siblings();
+    }
+
+    if (at.hasClass('thisAsideOpen')) {
+      atSibs.removeClass('thisAsideOpen');
+      at.removeClass('thisAsideOpen');
+      as.removeClass('asideOpen');
+      asSibs.removeClass('asideOpen');
+      aside.removeClass('asideOpen');
+    } else {
+      atSibs.removeClass('thisAsideOpen');
+      at.addClass('thisAsideOpen');
+      as.addClass('asideOpen');
+      asSibs.removeClass('asideOpen');
+      aside.addClass('asideOpen');
+    }
+}
+
+// Clicking navbar icons open the appropriate aside block
+$('body').on('click', '.asideToggle', function(){
+  if ($(this).is('#asideToggleActivities')) {
+    asideMenuToggle($('#asideToggleActivities'), 'activities');
+  } else if ($(this).is('#asideToggleTasks')) {
+    asideMenuToggle($('#asideToggleTasks'), 'tasks');
+  } else if ($(this).is('#asideToggleSettings')) {
+    asideMenuToggle($('#asideToggleSettings'), 'settings');
+  }
 })
 
 // Controls the custom checkboxes in the first section of the sidebar settings
