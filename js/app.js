6d4ff299293c25107e7b6d6ccbd8c4bbdbf771cb
Clayton Kinder <ClaytonAlanKinder@gmail.com>
2015-09-12 16:25:27 -0400

    Working on responsiveness.

--- js/app.js
+++ js/app.js
@@ -2,28 +2,18 @@
 
 $(window).load(function () {
 
-  $(function(){
-    $("#navbarInclude").load("./views/navbar.html");
-    $("#sideNavbarInclude").load("./views/side-navbar.html");
-    $("#headerInclude").load("./views/header.html");
-    $("#mainUsersInclude").load("./views/main-users.html");
-    $("#mainWeatherInclude").load("./views/main-weather.html");
-    $("#mainChatInclude").load("./views/main-chat.html");
-    $("#mainUsageInclude").load("./views/main-usage.html");
-    $("#asideActivitiesInclude").load("./views/aside-activities.html");
-    $("#asideSettingsInclude").load("./views/aside-settings.html");
-    $("#asideTasksInclude").load("./views/aside-tasks.html");
-  });
-
-  // Controls the initial color of the custom checkboxes in the first section of the
-  // sidebar settings
-  $('.slideCheckbox').each(function(){
-    if ($(this)[0].checked === true) {
-      $(this).closest('.slideCheckboxBlock').css('background-color', '#679900');
-    } else {
-      $(this).closest('.slideCheckboxBlock').css('background-color', '#CCCCCC');
-    }
-  });
+  // $(function(){
+  //   $("#navbarInclude").load("./views/navbar.html");
+  //   $("#sideNavbarInclude").load("./views/side-navbar.html");
+  //   $("#headerInclude").load("./views/header.html");
+  //   $("#mainUsersInclude").load("./views/main-users.html");
+  //   $("#mainWeatherInclude").load("./views/main-weather.html");
+  //   $("#mainChatInclude").load("./views/main-chat.html");
+  //   $("#mainUsageInclude").load("./views/main-usage.html");
+  //   $("#asideActivitiesInclude").load("./views/aside-activities.html");
+  //   $("#asideSettingsInclude").load("./views/aside-settings.html");
+  //   $("#asideTasksInclude").load("./views/aside-tasks.html");
+  // });
 });
 
 // Toggles the side-menu
@@ -41,9 +31,5 @@ $('body').on('click', '.sideNavbarMask', function(){
 
 // Controls the custom checkboxes in the first section of the sidebar settings
 $('body').on('change', '.slideCheckbox', function(){
-  if ($(this)[0].checked === true) {
-    $(this).closest('.slideCheckboxBlock').css('background-color', '#679900');
-  } else {
-    $(this).closest('.slideCheckboxBlock').css('background-color', '#CCCCCC');
-  }
+  $(this).closest('.slideCheckboxBlock').toggleClass('slideChecked');
 })
